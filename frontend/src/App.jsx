f2992a916e1129c710b30836f23706cb9e284450
vansh <krvansh30@gmail.com>
2025-02-08 13:59:46 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div className="text-3xl text-amber-600 font-bold underline">hello world</div>
+  );
+}
+
+export default App
